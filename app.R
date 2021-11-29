46e1338acb29e724758698d043120292ab87ec40
Tommy Whitney <46704616+ymmot7@users.noreply.github.com>
2021-11-29 14:22:37 -0700

    added some lidR functionality
    added RGL 3d viewer, right now the file it uses is hard coded. Need to use the user uploaded file.

--- app.R
+++ app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(lidR)
+library(shinyRGL)
 
 # maximum file size upload is 10 Gigs (10000*1024^2)
 options(shiny.maxRequestSize = 10000*1024^2)
@@ -9,7 +10,7 @@ ui <- fluidPage(
   # we need a name
   titlePanel("Lumberhack"),
   
- 
+  
   sidebarLayout(
     
     # start of stuff on sidebar
@@ -19,13 +20,19 @@ ui <- fluidPage(
       fileInput( inputId = "f",
                  label = "Choose .las or .laz file (max file size is 10 GB)",
                  accept = c(".laz", ".laz"),
-                 multiple = TRUE)),
-  
+                 multiple = TRUE),
+      
+      actionButton("submit", label = "Submit")
+    ),
+    
     # main viewing pane
     mainPanel(
-  
+      
       # shows the files that are loaded in a table format
-      tableOutput("table")
+      tableOutput("table"),
+      
+      # shows the RGL viewer for 3d point cloud
+      webGLOutput("RGL")
     )
   )
 )
@@ -38,7 +45,24 @@ server <- function(input, output) {
     req(input$f)
     input$f
   })
+  
+  
+  # DOES NOT WORK WITH USER FILE AS OF NOW
+  # create plot when submit button is pressed
+  plot_reactive <- eventReactive(input$submit, {
+    
+    # THIS NEEDS TO BE FIXED TO WORK WITH USER DATA
+    # input$f uploads as a data frame, not .laz/las file
+    las <- readLAS("results1.laz")
+    print(las)
+    lidR::plot(las)
+  })
+  
+  
+  # render WebGL for 3dplot
+  output$RGL <- renderWebGL({ 
+    plot_reactive()
+  })
 }
 
 shinyApp(ui = ui, server = server)
-
---
+++ test_lidR.R
@@ -0,0 +1,7 @@
+las <- readLAS('results.laz')
+print(las)
+lidR::plot(las)
+
+setwd("C:/Users/rexmy/Documents/GitHub/mobile_lidar")
+
+getwd()
