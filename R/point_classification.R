6b2f26b393b03873cc22b87d052e32891240dad3
Jenna Thanh Pedro <75430495+itsjennaz@users.noreply.github.com>
2022-02-08 20:22:56 -0700

    Update and rename classify_points.R to point_classification.R

--- R/point_classification.R
+++ R/point_classification.R
@@ -1,4 +1,4 @@
-# CLASSIFY GROUND 
+## CLASSIFY GROUND ## 
 
 # use progressive morphological filter
 # these parameters are for speed 
@@ -18,3 +18,23 @@ las <- classify_ground(las, mycsf)
 
 # if wanting to plot
 # plot(las, color = "Classification")
+
+## CLASSIFY CROWN ##
+## NEEDS WORK ## 
+
+# using k-means cluster for individual tree detection
+# set xyz coordinates and subset the data
+xyzi <- subset(las[,1:4], las[,3] >= 1.37)
+
+# find if there are any tree clusters in data
+tclas <- kmeans(xyzi[,1:2], 32)
+
+# set id vector on tree
+ID <- as.factor(tclas$cluster)
+
+# combining xyzi and tree id
+xyziID <- cbind(xyzi, ID)
+
+# compute individual tree crown metrics
+treeCrown <- CrownMetrics(xyziID)
+head(treeCrown)
