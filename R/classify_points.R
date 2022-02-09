7cecaa735eae23265e2a704297308c6ec5a57235
Jenna Thanh Pedro <75430495+itsjennaz@users.noreply.github.com>
2022-02-08 20:09:31 -0700

    Create classify_points.R

---
+++ R/classify_points.R
@@ -0,0 +1,20 @@
+# classify ground function
+
+# use progressive morphological filter
+# these parameters are for speed 
+ws <- seq(3, 12, 4)
+th <- seq(0.1, 1.5, length.out = length(ws))
+
+# using classify_ground function from LiDR package
+las <- classify_ground(las, pmf(ws, th))
+
+# if wanting to plot
+# plot(las, color = "Classification")
+
+# use cloth simulation filter
+# these parameters are also for speed
+mycsf <- csf(TRUE, 1, 1, time_step = 1)
+las <- classify_ground(las, mycsf)
+
+# if wanting to plot
+# plot(las, color = "Classification")
