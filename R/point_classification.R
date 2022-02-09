921d3528a49b8576718672a9f0e5b81f86529baf
Jenna Thanh Pedro <75430495+itsjennaz@users.noreply.github.com>
2022-02-09 16:43:13 -0700

    updating classify ground and crown

--- R/point_classification.R
+++ R/point_classification.R
@@ -1,6 +1,6 @@
 ## CLASSIFY GROUND ## 
 
-# use progressive morphological filter
+# use progressive morphological filter ; detects non-ground LIDAR measurements
 # these parameters are for speed 
 ws <- seq(3, 12, 4)
 th <- seq(0.1, 1.5, length.out = length(ws))
@@ -11,7 +11,7 @@ las <- classify_ground(las, pmf(ws, th))
 # if wanting to plot
 # plot(las, color = "Classification")
 
-# use cloth simulation filter
+# use cloth simulation filter ; separates point clouds into ground and non-ground measurments 
 # these parameters are also for speed
 mycsf <- csf(TRUE, 1, 1, time_step = 1)
 las <- classify_ground(las, mycsf)
